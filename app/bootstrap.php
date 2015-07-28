2ca710f060edc65cfab7c7a0773521678834055b
Peter Krejci <info@peterkrejci.sk>
2015-07-28 23:11:02 +0200

    disabled debug mode

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -4,8 +4,8 @@ require __DIR__ . '/../vendor/autoload.php';
 
 $configurator = new Nette\Configurator;
 
-$configurator->setDebugMode('192.168.1.100'); // enable for your remote IP
-$configurator->setDebugMode(true); // enable for your remote IP
+//$configurator->setDebugMode('192.168.1.100'); // enable for your remote IP
+//$configurator->setDebugMode(true); // enable for your remote IP
 $configurator->enableDebugger(__DIR__ . '/../log');
 
 $configurator->setTempDirectory(__DIR__ . '/../temp');
