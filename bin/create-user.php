7d84ef2dc57b82281d894446377357f1d4a8e343
Peter Krejci <info@peterkrejci.sk>
2015-07-28 23:13:12 +0200

    fixed fullname for new user

--- bin/create-user.php
+++ bin/create-user.php
@@ -9,10 +9,10 @@ Usage: create-user.php <name> <password>
 	exit(1);
 }
 
-list(, $name, $password) = $_SERVER['argv'];
+list(, $name, $password, $fullname) = $_SERVER['argv'];
 
 $container = require __DIR__ . '/../app/bootstrap.php';
 $manager = $container->getByType('App\Models\UserModel');
 
-$manager->add($name, $password);
+$manager->add($name, $password, $fullname);
 echo "User $name was added.\n";
